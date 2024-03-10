75cfe3383e625034b7ec459afa816fd05ceeba1b
rjgallac <rjgallac@hotmail.com>
2024-03-10 21:00:25 +0000

    convert from resttemplate to webclient

--- product-service/src/main/java/co/uk/microservicedemo/productservice/service/TestService.java
+++ product-service/src/main/java/co/uk/microservicedemo/productservice/service/TestService.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,9 +20,17 @@ import java.util.Random;
 public class TestService implements ITestService {
 
     RestTemplate restTemplate;
+    private WebClient webClient;
+
     public List<Product> get() {
-        ResponseEntity<List<Product>> exchange = restTemplate.exchange("http://inventory-service/api/inventory", HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
-        List<Product> products = exchange.getBody();
+//        ResponseEntity<List<Product>> exchange = restTemplate.exchange("http://inventory-service/api/inventory", HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+//        List<Product> products = exchange.getBody();
+
+        List<Product> products = webClient.get()
+                .uri("http://inventory-service/api/inventory")
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<Product>>() {
+                }).block();
         assert products != null;
         log.info("SUCCESS");
         return new ArrayList<>(products);
